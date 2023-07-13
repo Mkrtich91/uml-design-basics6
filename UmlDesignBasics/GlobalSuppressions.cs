ca21ccaaaab1860bf2553ef02ba2e98a6cd130c0
Mkrtich Kazanchian <mkogeo@gmail.com>
2023-07-13 21:18:17 +0400

    corrected

--- UmlDesignBasics/GlobalSuppressions.cs
+++ UmlDesignBasics/GlobalSuppressions.cs
@@ -1,8 +1,12 @@
-﻿// This file is used by Code Analysis to maintain SuppressMessage
+// This file is used by Code Analysis to maintain SuppressMessage
 // attributes that are applied to this project.
 // Project-level suppressions either have no target or are given
 // a specific target and scoped to a namespace, type, member, etc.
 
+#pragma warning disable SA1404
+
 using System.Diagnostics.CodeAnalysis;
 
 [assembly: SuppressMessage("CodeQuality", "IDE0079:Remove unnecessary suppression", Justification = "<Pending>")]
+
+#pragma warning restore SA1404
